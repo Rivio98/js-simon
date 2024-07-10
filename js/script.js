6c74e12dc460b7ea9ff3618d789d76bcd3429503
Rivio98 <rivaldogjoni1998@gmail.com>
2024-07-10 09:46:02 +0200

    [UPD] Added function for array

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//funzione per generare 5 numeri random
+function generateFiveNumbers(num) {
+    return Math.floor(Math.random() * 100 + 1);
+}
+
+//funzione per generare array dei numeri
+function createArray() {
+    let array = [];
+}
