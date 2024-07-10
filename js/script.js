52c95b820133514c2816e7e57bee4b82d71231e2
Rivio98 <rivaldogjoni1998@gmail.com>
2024-07-10 09:50:07 +0200

    [UPD] Defined function

--- js/script.js
+++ js/script.js
@@ -6,4 +6,15 @@ function generateFiveNumbers(num) {
 //funzione per generare array dei numeri
 function createArray() {
     let array = [];
+    //verifico la lunghezza array e se minore di 5 pusho il numero nell'array
+    while (numbers < 5) {
+        //genero il numero con la funzione di sopra 
+        let num = generateFiveNumbers();
+        //verifico se il numero è incluso nell'array
+        if (!numbers.includes(numbers)); {
+            //pusho il numero nell array
+            array.push(num);
+        }
+    }
+    return numbers;
 }
